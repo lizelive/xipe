2d4d87520d3bcd7ae09376c534116ffcce6db8e5
lizelive <github@lize.live>
2022-04-11 13:34:10 -0700

    Update dive.sh

--- scripts/dive.sh
+++ scripts/dive.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_API_VERSION=$(docker version --format '{{.Server.APIVersion}}') wagoodman/dive:latest "$@"
+docker run --rm -it \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-e DOCKER_API_VERSION=$(docker version --format '{{.Server.APIVersion}}') \
+-v "$(pwd)":"/host$(pwd)" \
+-w "/host$(pwd)" \
+wagoodman/dive:latest "$@"
